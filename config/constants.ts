c690bb79ab0c4693fba064cb3d168fed7ac417e9
WM <wmrngcd@gmail.com>
2025-04-07 22:22:17 +0300

    ✅ added env. variables

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,16 @@
 export const MODEL = "gpt-4o-mini";
 
+// Environment variables
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const AZURE_OPENAI_API_KEY = process.env.AZURE_OPENAI_API_KEY;
+export const AZURE_OPENAI_ENDPOINT = process.env.AZURE_OPENAI_ENDPOINT;
+// export const AZURE_OPENAI_DEPLOYMENT_NAME = process.env.AZURE_OPENAI_DEPLOYMENT_NAME;
+// export const AZURE_OPENAI_MODEL = process.env.AZURE_OPENAI_MODEL;
+export const COHERE_API_KEY = process.env.COHERE_API_KEY;
+// export const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+// export const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY;
+// export const MONGO_URI = process.env.MONGO_URI;
+
 // Developer prompt for the assistant
 export const DEVELOPER_PROMPT = `
 You are a helpful assistant helping users with their queries.
@@ -15,7 +26,7 @@ If they ask for something that is related to their own data, use the file search
 
 // Initial message that will be displayed in the chat
 export const INITIAL_MESSAGE = `
-Hi, how can I help you?
+What's up? 😄
 `;
 
 export const defaultVectorStore = {
